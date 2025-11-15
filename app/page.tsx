b740a9d381dd052513fde67ebe91e054b03aac67
Cedricoins <111881437+Cedricoins@users.noreply.github.com>
2025-11-15 17:10:13 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import CatTranslator from "@/components/CatTranslator";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <CatTranslator />
+    </main>
+  );
+}
